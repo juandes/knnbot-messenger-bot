a54b4bf076530f80fb9e002ecb3bc76abc9822e2
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2016-12-14 20:12:15 +0100

    added webhook

--- app.py
+++ app.py
@@ -41,9 +41,34 @@ def main():
                 print 'Predicted label=' + str(majority_vote) + ', Actual label=' + str(to_predict[x])
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def landing():
-    return ACCESS_TOKEN[1:4]
+    return 'Hi.'
+
+@app.route("/", methods=['POST'])
+def webhook():
+    if request.method == 'GET':
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        else:
+            return 'Invalid verification token'
+
+    if request.method == 'POST':
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for x in messaging:
+                if x.get('message'):
+                    recipient_id = x['sender']['id']
+                    if x['message'].get('text'):
+                        message = x['message']['text']
+                        bot.send_text_message(recipient_id, message)
+                    if x['message'].get('attachment'):
+                        bot.send_attachment_url(recipient_id, x['message']['attachment']['type'],
+                                                x['message']['attachment']['payload']['url'])
+                else:
+                    pass
+        return "Success"
 
 if __name__ == '__main__':
     app.run(debug=True)
