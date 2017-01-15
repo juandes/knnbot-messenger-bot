e846857f9046b6b5d02f6dbe10d050f85b9384a8
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2017-01-15 14:15:14 +0100

    First predict test successful. Knnbot works

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
+#ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 VERIFY_TOKEN = "bot_verify"
 bot = Bot(ACCESS_TOKEN)
 
@@ -21,7 +22,7 @@ def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -64,10 +65,12 @@ training_set = []
 train = defaultdict(list)
 
 a = []
+PREDICT_STATE = 1
+user_state = {}
 
 
-@app.route("/", methods=['POST'])
-def webhook():
+@app.route("/2", methods=['POST'])
+def webhook2():
     if request.method == 'POST':
         output = request.get_json()
         for event in output['entry']:
@@ -80,12 +83,8 @@ def webhook():
     return 'Success'
 
 
-@app.route("/2", methods=['POST', 'GET'])
-def webhook2():
-    if request.method == 'GET':
-        print('GET received')
-        print(request.args)
-
+@app.route("/", methods=['POST'])
+def webhook():
     if request.method == 'POST':
         output = request.get_json()
         print("POST received: {}".format(output))
@@ -101,6 +100,19 @@ def webhook2():
                     if x['message'].get('text'):
                         raw_input = x['message'].get('text')
                         input = raw_input.split(',')
+                        if user_state.get(recipient_id) == PREDICT_STATE:
+                            train_to_fit = np.array(train[recipient_id])
+                            to_predict = [(int(input[0]), int(input[1]))]
+                            to_predict = np.array(to_predict)
+
+                            neighbors = get_neighbors(
+                                training_set=train_to_fit, test_instance=to_predict[0], k=k)
+                            majority_vote = get_majority_vote(neighbors)
+                            print 'Predicted label=' + str(majority_vote)
+                            message = "Predicted label {}".format(
+                                str(majority_vote))
+                            bot.send_text_message(recipient_id, message)
+                            continue
                         if len(input) != 3:
                             message = 'Wrong input'
                             bot.send_text_message(recipient_id, message)
@@ -109,7 +121,6 @@ def webhook2():
                         training_input = (
                             (int(input[0]), int(input[1])), int(input[2]))
                         train[recipient_id].append(training_input)
-                        # training_set.append(training_input)
                         print(len(train[recipient_id]))
                         message = 'Input: {} accepted as training. Entry #{}'.format(
                             input, len(train[recipient_id]))
@@ -131,7 +142,10 @@ def webhook2():
                 elif x.get('postback'):
                     recipient_id = x['sender']['id']
                     payload = x['postback'].get('payload')
-                    bot.send_text_message(recipient_id, payload)
+                    if payload == 'YES_USE_KNN':
+                        user_state[recipient_id] = PREDICT_STATE
+                        bot.send_text_message(
+                            recipient_id, "Enter example to predict")
                     pass
                 else:
                     pass
@@ -139,4 +153,5 @@ def webhook2():
 
 
 if __name__ == '__main__':
+    print('token ' + os.environ.get('ACCESS_TOKEN'))
     app.run(debug=True)
